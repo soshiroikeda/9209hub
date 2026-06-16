6d13dd608cd52411ac8102adadea7c0c7bb0f81e
hirapito <soshiroikeda0624@gmail.com>
2026-06-16 20:04:15 +0900

    原型完成

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log('This website is still under construction.');
+console.log('We are hiring developers!');
+console.log('If you are interested, please contact us at the email address below.');
+console.log('%cdev@9209.org','font-size:25px');
